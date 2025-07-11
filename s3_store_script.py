ff4bd46f3388b4d71574170031f2bb137123af3d
Husnain <husnainhere.2019.5@gmail.com>
2025-07-11 16:32:54 +0500

    Add script to upload local file to S3

---
+++ s3_store_script.py
@@ -0,0 +1,27 @@
+"""
+Script to take create a s3 bucket and store file from local storage to AWS S3
+"""
+import boto3
+
+bucket_name = "backup-storage-husnain-20250711" #Enter any unique name here or use pre existing bucket 
+region = "us-east-2" #Enter your own region
+s3 = boto3.resource("s3", region_name=region)
+
+def create_bucket(s3,bucket_name,region):
+    s3.create_bucket(
+        Bucket=bucket_name,
+        CreateBucketConfiguration={'LocationConstraint': region}
+    )
+    print("Bucket Created Successfully")
+
+def upload_backup(s3,file_name,bucket_name,key_name):
+    data = open(file_name, 'rb') #convert data to binary 
+    s3.Bucket(bucket_name).put_object(Key=key_name, Body=data)
+    print("Backup uploaded successfully")
+
+
+create_bucket(s3,bucket_name,region) #No need to run this function if you are using pre existing S3 bucket
+
+file_path = "File Path here" #Copy path to your file here
+file_name = "my-backup.zip" #You can choose a different name
+upload_backup(s3,file_name,bucket_name,file_name) 
